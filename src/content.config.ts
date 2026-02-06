5557dc6c341f98c09e920e55c0481d45bdbffebe
Simone Rosani <rsn.smn@gmail.com>
2026-02-06 15:35:56 +0100

    Implement dynamic generation of blog routes and pages.

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,6 +8,7 @@ const blog = defineCollection({
     date: z.coerce.date(),
     description: z.string(),
     tags: z.array(z.string()).optional(),
+    thumbnail: z.string().optional(),
   }),
 });
 
