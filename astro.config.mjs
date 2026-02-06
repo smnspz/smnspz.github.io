3321b0212d86febb9c00e12a56a63c670a1e68e9
Simone Rosani <rsn.smn@gmail.com>
2026-02-06 16:20:46 +0100

    Add github workflow for deploying on pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // GitHub Pages deployment
+  site: 'https://srosani.github.io',
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
